728fe644cf8fba6dcd9461ce7a13ff00abfe095d
NidhiSingh-25 <nidhi630694@gmail.com>
2025-10-01 17:47:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 function insert_Row() {
     //Write your code here
-	let table=document.getElementById("sampleTable");
-	let newRow=table.insertRow();
+	let table = document.getElementById("sampleTable");
 
-	let cell1=newRow.insertCell(0);
-	let cell2=newRow.insertCell(1);
+    // Find how many rows are already there
+    let rowCount = table.rows.length;
 
-	cell1.innerHTML = "New Cell1";
-	cell2.innerHTML = "New Cell2";
- 
+    // Insert a new row at the end
+    let newRow = table.insertRow();
+
+    // Insert two new cells
+    let cell1 = newRow.insertCell(0);
+    let cell2 = newRow.insertCell(1);
+
+    // Set text using the row count + 1 (for next row number)
+    cell1.innerHTML = "Row" + (rowCount + 1) + " cell1";
+    cell2.innerHTML = "Row" + (rowCount + 1) + " cell2";
 }
